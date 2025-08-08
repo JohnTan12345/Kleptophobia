814eee5014e9c073b3e35a0f2f03cb70db64bdd5
10267815E <s10267815@connect.np.edu.sg>
2025-08-08 13:53:41 +0800

    implemented basic door behaviour turning script, but pivot point is off

--- Assets/Scripts/DoorBehaviour.cs
+++ Assets/Scripts/DoorBehaviour.cs
@@ -12,19 +12,59 @@ public class DoorBehaviour : MonoBehaviour
     public Transform door;           // door
     public float openAngle = 90f;    // How far the door opens
     public float openSpeed = 2f;     // Speed of door rotation
-    public bool playerNearby = false;
+    public bool playerNearby = false; // Whether the player is nearby to trigger door opening
 
     private Vector3 closedRotation;  // Original rotation of the door
     private Vector3 openRotation;    // Target rotation when door is open
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        // Save the closed rotation
+        closedRotation = door.eulerAngles;
+
+        // Calculate what the open rotation will be
+        openRotation = new Vector3(
+            closedRotation.x,
+            closedRotation.y + openAngle,
+            closedRotation.z
+        );
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        // Smoothly rotate door depending on whether player is nearby
+        if (playerNearby)
+        {
+            door.eulerAngles = Vector3.Lerp(
+                door.eulerAngles,
+                openRotation,
+                Time.deltaTime * openSpeed
+            );
+        }
+        else
+        {
+            door.eulerAngles = Vector3.Lerp(
+                door.eulerAngles,
+                closedRotation,
+                Time.deltaTime * openSpeed
+            );
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player")) // Check if the object entering the trigger is the player
+        {
+            playerNearby = true; // Set playerNearby to true to open the door
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player")) // Check if the object exiting the trigger is the player
+        {
+            playerNearby = false; // Set playerNearby to false to close the door
+        }
     }
 }
